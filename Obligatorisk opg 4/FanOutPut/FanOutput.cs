107b705b2793c86d1f5534d74f6052eb8c4d6a20
Maja Seehusen Bloch <maja8202@edu.easj.dk>
2020-10-07 09:50:40 +0200

    ObligatoriskOpg4

---
+++ Obligatorisk opg 4/FanOutPut/FanOutput.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace FanLibrary
+{
+    public class FanOutput
+    {
+
+        private int _id;
+        private string _navn;
+        private double _temp;
+        private double _fugt;
+
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                CheckId(value);
+                _id = value;
+            }
+        }
+
+        public string Navn
+        {
+            get => _navn;
+            set
+            {
+                CheckNavn(value);
+                _navn = value;
+            }
+        }
+
+        public double Temp
+        {
+            get => _temp;
+            set
+            {
+                CheckTemp(value);
+                _temp = value;
+            }
+        }
+
+        public double Fugt
+        {
+            get => _fugt;
+            set
+            {
+                CheckFugt(value);
+                _fugt = value;
+            }
+        }
+
+        public FanOutput(int _id, string _navn, double _temp, double _fugt)
+        {
+            Id = _id;
+            Navn = _navn;
+            Temp = _temp;
+            Fugt = _fugt;
+
+        }
+
+
+        public FanOutput()
+        {
+
+        }
+
+        public static void CheckId(int id)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentException("Id skal være et positivt tal");
+            }
+
+        }
+
+        //Navn, en tekst-streng, min 2 karakterer lang
+        public void CheckNavn(string navn)
+        {
+            if (navn.Length < 2)
+            {
+                throw new ArgumentException("Navn skal være på 2 eller flere bogstaver");
+            }
+        }
+
+        //Temp et tal mellem 15 og 25 (målt i grader celsius, 15 <= Temp <= 25) 
+        public void CheckTemp(double temp)
+        {
+            if (temp < 15 || temp > 25)
+            {
+                throw new ArgumentOutOfRangeException("temp", temp, "temp er ikke indefor godkendt område");
+                //throw new ArgumentException("temp er ikke indefor godkendt område");
+            }
+        }
+
+        //Fugt et tal mellem 30 og 80 (relativ fugtighed, 30 <= Fugt <= 80)
+        public void CheckFugt(double fugt)
+        {
+            if (fugt < 30 || fugt > 80)
+            {
+                throw new ArgumentOutOfRangeException("fugt", fugt, "fugt er ikke indefor godkendt område");
+                //throw new ArgumentException("fugt er ikke indefor godkendt område");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {_id}, Navn: {_navn}, Temp: {_temp}, Fugt: {_fugt}, Id: {Id}";
+        }
+
+    }
+}
---
+++ Obligatorisk opg 4/Obligatorisk opg 4/Controllers/FanOutPutController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FanLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Obligatorisk_opg_4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FanOutPutController : ControllerBase
+    {
+        private static int nextId = 3;
+
+        private static readonly List<FanOutput> measurments = new List<FanOutput>()
+        {
+            new FanOutput(1, "AZ",21,40),
+            new FanOutput(2, "BF", 15,51)
+
+        };
+
+        // GET: api/<FanOutPutController>
+        [HttpGet]
+        public IEnumerable<FanOutput> Get()
+        {
+            return measurments;
+        }
+
+        // GET api/<FanOutPutController>/5
+        [HttpGet("{id}")]
+        public FanOutput Get(int id)
+        {
+            return measurments.Find(i => i.Id == id);
+        }
+
+        // POST api/<FanOutPutController>
+        [HttpPost]
+        public void Post([FromBody] FanOutput value)
+        {
+            value.Id = nextId++;
+            measurments.Add(value);
+        }
+
+        // PUT api/<FanOutPutController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] FanOutput value)
+        {
+            FanOutput item = Get(id);
+            if (item != null)
+            {
+
+                item.Id = value.Id;
+                item.Navn = value.Navn;
+                item.Temp = value.Temp;
+                item.Fugt = value.Fugt;
+            }
+        }
+
+        // DELETE api/<FanOutPutController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FanOutput item = Get(id);
+            measurments.Remove(item);
+        }
+    }
+}
